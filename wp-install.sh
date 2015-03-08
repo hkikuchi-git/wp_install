79b7215c89074ee7ee6fd3482163d8882b2e0e7f
KIKUCHI Hidekazu <hkikuchi@miew.co.jp>
2015-03-08 15:43:22 +0900

    base script.

---
+++ wp-install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash -e
+
+# reference
+#  https://gist.github.com/bgallagh3r/2853221
+
+clear
+echo "============================================"
+echo "WordPress Install Script"
+echo "============================================"
+echo "Database Name: "
+read -e dbname
+echo "Database User: "
+read -e dbuser
+echo "Database Password: "
+read -s dbpass
+echo "run install? (y/n)"
+read -e run
+if [ "$run" == n ] ; then
+exit
+else
+echo "============================================"
+echo "A robot is now installing WordPress for you."
+echo "============================================"
+#download wordpress
+curl -O https://wordpress.org/latest.tar.gz
+#unzip wordpress
+tar -zxvf latest.tar.gz
+#change dir to wordpress
+cd wordpress
+#copy file to parent dir
+cp -rf . ..
+#move back to parent dir
+cd ..
+#remove files from wordpress folder
+rm -R wordpress
+#create wp config
+cp wp-config-sample.php wp-config.php
+#set database details with perl find and replace
+perl -pi -e "s/database_name_here/$dbname/g" wp-config.php
+perl -pi -e "s/username_here/$dbuser/g" wp-config.php
+perl -pi -e "s/password_here/$dbpass/g" wp-config.php
+#create uploads folder and set permissions
+mkdir wp-content/uploads
+chmod 777 wp-content/uploads
+#remove zip file
+rm latest.tar.gz
+#remove bash script
+rm wp.sh
+echo "========================="
+echo "Installation is complete."
+echo "========================="
+fi
