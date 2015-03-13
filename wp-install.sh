ea8a4625c5340cc736f4c0171812db73f70ee9ca
KIKUCHI Hidekazu <hkikuchi@miew.co.jp>
2015-03-13 20:55:25 +0900

    Create WordPress-DB.

--- wp-install.sh
+++ wp-install.sh
@@ -9,6 +9,13 @@ _WP_FILE=latest-ja.tar.gz
 _WP_DL=${_WP_SITE}${_WP_FILE}
 
 #
+#
+_MYSQL_HOST="localhost"
+_MYSQL_USER="root"
+_MYSQL_PW=""
+
+
+#
 _LEN=32
 
 #
@@ -19,6 +26,7 @@ fi
 
 _DIR=$1
 
+# make install-directory
 if [ ! -d ${_DIR} ]; then
   mkdir -p ${_DIR}
 fi
@@ -39,8 +47,23 @@ read -s dbpass
 echo "run install? (y/n)"
 read -e run
 if [ "$run" == n ] ; then
-exit
-else
+  exit 0
+fi
+
+echo "============================================"
+echo "Database Setup"
+echo "============================================"
+
+echo "CREATE DATABASE $dbname CHARACTER SET utf8;" > mysql_cmd.sql
+echo "GRANT ALL PRIVILEGES ON ${dbname}.* TO ${dbuser}@localhost IDENTIFIED BY '$dbpass';" >> mysql_cmd.sql
+echo "FLUSH PRIVILEGES;" >> mysql_cmd.sql
+echo "exit" >> mysql_cmd.sql
+
+mysql -u ${_MYSQL_USER} -p${_MYSQL_PW} < mysql_cmd.sql
+
+rm -f mysql_cmd.sql
+
+
 echo "============================================"
 echo "A robot is now installing WordPress for you."
 echo "============================================"
@@ -49,26 +72,32 @@ curl -O -o ${_WP_FILE} ${_WP_DL}
 #unzip wordpress
 tar -zxvf ${_WP_FILE}
 
-#change dir to wordpress
-cd wordpress
-#copy file to parent dir
-cp -rf . ..
-#move back to parent dir
-cd ..
-#remove files from wordpress folder
-rm -R wordpress
+#change directory-name
+mv wordpress wp
+#move directory
+cd wp
+
 #create wp config
 cp wp-config-sample.php wp-config.php
+
 #set database details with perl find and replace
 perl -pi -e "s/database_name_here/$dbname/g" wp-config.php
 perl -pi -e "s/username_here/$dbuser/g" wp-config.php
 perl -pi -e "s/password_here/$dbpass/g" wp-config.php
+
 #create uploads folder and set permissions
 mkdir wp-content/uploads
 chmod 777 wp-content/uploads
+
+#move back to parent dir
+cd ..
+
 #remove zip file
 rm ${_WP_FILE}
+
 echo "========================="
 echo "Installation is complete."
 echo "========================="
-fi
+
+exit 1
+
