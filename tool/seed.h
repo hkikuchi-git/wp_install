e0c497818890dda90f6fc4cf0fea8da6265bbe79
KIKUCHI Hidekazu <hkikuchi@miew.co.jp>
2015-03-15 14:42:36 +0900

    出力される文字の調整

--- tool/seed.h
+++ tool/seed.h
@@ -6,9 +6,9 @@
 #ifndef _RANDOM_SEED_H_
 #define _RANDOM_SEED_H_
 
-#define RND_SEED1	("abcdefghjmnqrty")
-#define RND_SEED2	("ABCDEFGHJKLMNPQRSTUVWXY")
-#define RND_NUM		("023456789.-=#%*")
+#define RND_SEED1	("abcdefghijklmnopqrstuvwxyz")
+#define RND_SEED2	("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+#define RND_NUM		("0123456789?.-=#%*")
 #define	RND_KEY		("%#=-+?@")
 
 #define	SEED_LEN	(32)
